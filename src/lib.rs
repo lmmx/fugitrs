9ca14b41004c25f89a72975e202849a41c2ca75a
Louis Maddox <louismmx@gmail.com>
2023-12-24 16:16:49 +0000

    feat: turn new lines green

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 use git2::{Repository, DiffFormat, DiffLineType};
 
 #[pyfunction]
-fn get_git_diff(repo_path: String) -> PyResult<()> {
+fn get_git_diff(repo_path: String, plain: Option<bool>) -> PyResult<()> {
     let repo = Repository::open(repo_path).expect("Failed to open repository");
 
     // Get the tree of the current HEAD commit
@@ -16,15 +16,24 @@ fn get_git_diff(repo_path: String) -> PyResult<()> {
     let diff = repo.diff_tree_to_workdir_with_index(Some(&head_tree), None)
                    .expect("Failed to create diff");
 
-    // Print the diff with line prefixes
-    diff.print(DiffFormat::Patch, |_delta, _hunk, line| {
+    // Check if plain output is requested
+    let is_plain = plain.unwrap_or(false);
+
+    // Print the diff with line prefixes and optional color
+    diff.print(DiffFormat::Patch, move |_delta, _hunk, line| {
         let line_content = std::str::from_utf8(line.content()).unwrap_or("");
-        let line_prefix = match line.origin() {
-            DiffLineType::Addition => "+",
-            DiffLineType::Deletion => "-",
-            _ => " ",
+        let (line_prefix, maybe_color_code, maybe_reset_code) = match line.origin() {
+            DiffLineType::Addition => {
+                if is_plain {
+                    ("+", "\x1b[32m", "\x1b[0m") // Green color for added lines
+                } else {
+                    ("+", "", "")
+                }
+            },
+            DiffLineType::Deletion => ("-","", ""),
+            _ => (" ", "", ""),
         };
-        println!("{}{}", line_prefix, line_content);
+        println!("{}{}{}{}", maybe_color_code, line_prefix, line_content, maybe_reset_code);
         true
     }).expect("Failed to print diff");
 
